5a934d6901ced476b118f802124e0a559bcab20a
ronnystar564 <163534333+ronnystar564@users.noreply.github.com>
2024-06-17 19:11:34 +0100

    done

--- API/client/src/app/nav/nav.component.ts
+++ API/client/src/app/nav/nav.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../Services/account.service';
+import { response } from 'express';
+import { error } from 'console';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, BrowserAnimationsModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
+  
 })
 export class NavComponent {
 
+  model: any = {}
+  logedIn = false;
+
+  constructor(private accountServices: AccountService) {
+
+  }
+  login() {
+    this.accountServices.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.logedIn = true;
+      },
+      error: error => console.log(error)
+    })
+  }
+
+  logout() {
+    this.logedIn = false;
+  }
+
 }
+
+
